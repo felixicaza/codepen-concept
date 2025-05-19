a8fce4bcb121c58b4c38a18e1961054df61c0d6b
Felix Icaza <fx.joliett17@gmail.com>
2025-05-19 16:54:21 -0600

    chore: migrate Astro config from JavaScript to TypeScript

---
+++ astro.config.ts
@@ -0,0 +1,93 @@
+import { defineConfig } from 'astro/config'
+
+import { URL } from './src/data/constants'
+
+import tunnel from 'astro-tunnel'
+import icon from 'astro-icon'
+import sitemap from 'astro-sitemap'
+import playformInline from '@playform/inline'
+import playformCompress from '@playform/compress'
+import compressor from 'astro-compressor'
+
+// https://astro.build/config
+export default defineConfig({
+  site: URL,
+  trailingSlash: 'never',
+  server: {
+    host: true
+  },
+  compressHTML: false,
+  vite: {
+    css: {
+      transformer: 'lightningcss'
+    }
+  },
+  integrations: [
+    tunnel(),
+    icon(),
+    sitemap({
+      canonicalURL: URL,
+      lastmod: new Date(),
+      createLinkInHead: false,
+      xmlns: {
+        xhtml: true,
+        image: false,
+        news: false,
+        video: false
+      },
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es'
+        }
+      },
+      // Remove trailing slash
+      serialize(item) {
+        item.url = item.url.replace(/\/$/g, '')
+        return item
+      }
+    }),
+    playformInline(),
+    playformCompress({
+      HTML: {
+        'html-minifier-terser': {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          maxLineLength: 0,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true,
+          noNewlinesBeforeTagClose: true,
+          removeAttributeQuotes: false,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          sortAttributes: true,
+          sortClassName: true,
+          useShortDoctype: true
+        }
+      },
+      JavaScript: {
+        terser: {
+          compress: {
+            arguments: true,
+            drop_console: true
+          },
+          format: {
+            comments: false,
+            indent_level: 2
+          },
+          ecma: 2020
+        }
+      },
+      Cache: true,
+      CSS: false,
+      Image: false,
+      SVG: false
+    }),
+    compressor()
+  ]
+})
