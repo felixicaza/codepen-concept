5be6b86da24cbca89beb99f727586463e361fae2
Felix Icaza <fx.joliett17@gmail.com>
2025-05-25 14:20:02 -0600

    feat: add toolbar options

---
+++ src/data/toolbar-options.ts
@@ -0,0 +1,53 @@
+const toolbar = [
+  {
+    width: 16,
+    height: 18,
+    name: 'preview/delete',
+    label: 'Delete',
+    isVisible: false
+  },
+  {
+    width: 19,
+    height: 18,
+    name: 'preview/embed',
+    label: 'Embed',
+    isVisible: false
+  },
+  {
+    width: 14,
+    height: 16,
+    name: 'preview/share',
+    label: 'Share',
+    isVisible: false
+  },
+  {
+    width: 16,
+    height: 16,
+    name: 'preview/collection',
+    label: 'Add to Collection',
+    isVisible: false
+  },
+  {
+    width: 15,
+    height: 16,
+    name: 'preview/fork',
+    label: 'Fork',
+    isVisible: false
+  },
+  {
+    width: 16,
+    height: 16,
+    name: 'preview/window',
+    label: 'Open new window',
+    isVisible: false
+  },
+  {
+    width: 15,
+    height: 16,
+    name: 'preview/export',
+    label: 'Export',
+    isVisible: false
+  }
+]
+
+export default toolbar
