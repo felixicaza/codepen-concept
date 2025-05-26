bc274eb2f9b73c53fabd460075ccfc2b3f9a739f
Felix Icaza <fx.joliett17@gmail.com>
2025-05-26 12:16:06 -0600

    feat: create user menu dropdown

---
+++ src/data/user-menu.ts
@@ -0,0 +1,75 @@
+const userMenu = [
+  {
+    label: 'Your Work',
+    icon: 'user-menu/work',
+    width: 16,
+    height: 15
+  },
+  {
+    label: 'Profile',
+    icon: 'user-menu/profile',
+    width: 14,
+    height: 19
+  },
+  {
+    label: 'New Pen',
+    icon: 'user-menu/pen',
+    width: 16,
+    height: 16,
+    hasSeparator: true
+  },
+  {
+    label: 'New Collection',
+    icon: 'user-menu/collection',
+    width: 16,
+    height: 16
+  },
+  {
+    label: 'Asset Manager',
+    icon: 'user-menu/asset',
+    width: 16,
+    height: 16,
+    hasSeparator: true
+  },
+  {
+    label: 'Embed Theme Builder',
+    icon: 'user-menu/embed',
+    width: 16,
+    height: 16
+  },
+  {
+    label: 'Upgrade',
+    icon: 'user-menu/pro',
+    width: 16,
+    height: 16,
+    isPro: true
+  },
+  {
+    label: 'Docs',
+    icon: 'user-menu/docs',
+    width: 13,
+    height: 16,
+    hasSeparator: true
+  },
+  {
+    label: 'Support',
+    icon: 'user-menu/support',
+    width: 16,
+    height: 16
+  },
+  {
+    label: 'Settings',
+    icon: 'user-menu/settings',
+    width: 16,
+    height: 16,
+    hasSeparator: true
+  },
+  {
+    label: 'Log Out',
+    icon: 'user-menu/logout',
+    width: 16,
+    height: 16
+  }
+]
+
+export default userMenu
